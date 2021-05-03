0c7c7b4a32f95ed8d348b6319e7b49337ffc41bb
Pierre-emmanuel Dacquet <pierre-emmanueldacquet@MacBook-Air-de-Pierre-emmanuel.local>
2021-05-03 14:40:54 +0200

    moved package.json, created package-lock.jsson,modified Docker-compose.yml and Dockerfile

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const { Router } = require('express');
 const express = require('express');
 const app = express();
 
 //app.get()
+
 
 app.listen(3000, () => {
     console.log ('Running on port 3000 !')
