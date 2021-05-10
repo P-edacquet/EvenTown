fe1bb08e68cf9df231ab84f8b4f06ad794ef574b
m_costa3@etu-webschoolfactory.fr <PTg]m3vH6prx7QNGU2}823]}%yd2>
2021-05-10 19:55:47 +0200

    feat: add Pin model and error middleware

--- server/server.js
+++ server/server.js
@@ -1,22 +1,46 @@
 require('dotenv').config();
 const express = require('express');
-const connectToDB = require('./database/db')
+const connectToDB = require('./database/db');
+const ErrorsMiddleware = require('./middleware/errorMiddleware');
+const PinError = require('./utils/PinError');
+
+
+process.on("uncaughtException", (error) => {
+    console.log("Uncaught Exception..... stopping the server....");
+    console.log(error.name, error.message);
+    process.exit(1);
+});
+
 
 const app = express();
-
-connectToDB();
-
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+connectToDB();
 
+//Routes
 app.get('/test', (req, res) => {
     res.json({
         Hi: "Welcome"
     });
 });
 
-app.listen(
+//Errors on routes using Error middleware
+app.all("*", ((req, res, next) => {
+    next(new PinError(`Can't find ${req.originalUrl} on this server!`, 404));
+}));
+app.use(ErrorsMiddleware);
+
+const server = app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+
+process.on("unhandledRejection", (error) => {
+    console.log("unhandledRejection..... stopping the server....");
+    console.log(error.name, error.message);
+    server.close(() => {
+        process.exit(1);
+    });
+});
