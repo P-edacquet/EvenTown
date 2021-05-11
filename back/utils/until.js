7fd0381a1932b03924adced58708fd705803cef9
m_costa3@etu-webschoolfactory.fr <PTg]m3vH6prx7QNGU2}823]}%yd2>
2021-05-11 18:59:48 +0200

    branch cleaned

--- back/utils/until.js
+++
@@ -1,9 +0,0 @@
-module.exports = (promise) => {
-    return promise
-        .then(res => {
-            return [res, null]
-        })
-        .catch(err => {
-            return [null, err]
-        })
-}
