13b752ecea135716de9a88d5f64b2a0df2cc1fe8
Pierre-emmanuel Dacquet <pierre-emmanueldacquet@MacBook-Air-de-Pierre-emmanuel.local>
2021-05-24 14:33:22 +0200

    modify heroku's port

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static(__dirname + '/client/build'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 80;
 connectToDB();
 
 //Routes
